755cc8a21f5d33cfdfdf6b573e31f722dabd6f09
kaustubh-ronge <kaustubhronge25@gmail.com>
2026-03-27 12:37:02 +0530

    created api routes

--- app/api/reports/[id]/route.js
+++ app/api/reports/[id]/route.js
@@ -1,12 +1,3 @@
-// import { NextResponse } from 'next/server';
-// import { getReportByReportId } from '@/actions/reportActions';
-
-// export async function GET(request, { params }) {
-//     const { id } = params;
-//     const result = await getReportByReportId(id);
-//     return NextResponse.json(result);
-// }
-
 
 import { NextResponse } from 'next/server';
 import { getReportByReportId } from '@/actions/reportActions';
